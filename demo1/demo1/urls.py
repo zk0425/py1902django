bc0738161e2d25d477f61479e5b0c9ffa6e25896
Administrator <zk161162@163.com>
2019-05-19 20:46:55 +0800

    django的投票项目

--- demo1/demo1/urls.py
+++ demo1/demo1/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     url('admin/', admin.site.urls),
     # 应用路由
     url('myapp1/', include("myapp1.urls", namespace="root")),
-    url('myapp2/', include("myapp2.urls")),
+    url('myapp2/', include("myapp2.urls",namespace="roos")),
     url('polls/', include("polls.urls")),
 ]
