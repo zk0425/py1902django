b8aa244969c4ec1b2bfd7c1a14986a71eac6c4e4
Administrator <zk161162@163.com>
2019-05-31 11:00:52 +0800

    demo3项目

--- demo3/demo3/settings.py
+++ demo3/demo3/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5a7qr+4pt=6k)2yz=^uc&5k0oi6qj7@o4vzmr@@(9ed*ym7y#r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
